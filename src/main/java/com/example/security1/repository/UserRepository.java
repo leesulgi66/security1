59aac83038214e9f3c5fcb21828a06c71c24e2a3
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-16 22:06:56 +0900

    시큐리티 로그인

--- src/main/java/com/example/security1/repository/UserRepository.java
+++ src/main/java/com/example/security1/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //CRUD 함수를 JpaRepository가 들고있음
 //JpaRepository을 상속 받았기 때문에 자동으로 Bean등록이 됨.
 public interface UserRepository extends JpaRepository<User, Integer> {
+    //findBy규칙 -> Username 문법
+    //select * from user where username = 1?
+    User findByUsername(String username);
 }
