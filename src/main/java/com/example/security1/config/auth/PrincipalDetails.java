7319ace8f4ff63ee0dcb52131c2c4e36d8e849d1
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-20 00:55:24 +0900

    Authentication객체가 가질 수 있는 2가지 타입

--- src/main/java/com/example/security1/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/security1/config/auth/PrincipalDetails.java
@@ -9,18 +9,32 @@ package com.example.security1.config.auth;
 // Security Session => Authentication => UserDetails(PrincipalDetails)
 
 import com.example.security1.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user; //콤포지션
 
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
     }
 
     //해당 User의 권한을 리턴하는곳!
--- src/main/java/com/example/security1/controller/IndexController.java
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -1,11 +1,16 @@
 package com.example.security1.controller;
 
+import com.example.security1.config.auth.PrincipalDetails;
 import com.example.security1.model.User;
 import com.example.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +23,26 @@ public class IndexController {
     private UserRepository userRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/test/login")
+    public @ResponseBody String testLogin(Authentication authentication, @AuthenticationPrincipal PrincipalDetails userDetails) { //authentication을 DI(의존성 주입)
+        System.out.println("/test/login =========================");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal(); // userDetails 타입으로 받아야 하지만, PrincipalDetails가 userDetails를 상속받았기 때문에 다운캐스팅 가능.
+        System.out.println("authentication : " + principalDetails.getUser());
+
+        System.out.println("userDetails : "+userDetails.getUser());
+        return "세션 정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oauth) { //authentication을 DI(의존성 주입)
+        System.out.println("/test/login =========================");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication : " + oAuth2User.getAttributes());
+        System.out.println("oauth2User : " + oauth.getAttributes());
+
+        return "OAuth 세션 정보 확인하기";
+    }
 
     //localhost:8080/
     //localhost:8080
