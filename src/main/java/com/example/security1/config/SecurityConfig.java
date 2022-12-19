df42e24240adb174a91e81e15f702dc4b3f4d8ca
leesulgi <107823688+leesulgi66@users.noreply.github.com>
2022-12-19 19:34:30 +0900

    구글회원 프로필정보 받아보기

--- src/main/java/com/example/security1/config/SecurityConfig.java
+++ src/main/java/com/example/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.security1.config;
 
+import com.example.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//1.코드받기(인증), 2.엑세스토큰(권한), 3.사용자프로필 정보를 가져오고, 4-1. 그 정보를 토대로 회원가입을 자동진행.
+//4-2. 기존 정보(이메일,전화번호,이름,아이디) + 추가적인 정보를 더 받아서 회원가입을 따로 진행.
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)// secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
@@ -36,7 +44,9 @@ public class SecurityConfig {
                 //.usernameParameter("username2") // PrincipalDetailsService의 username의 파라미터이름을 바꿀 때 사용
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함.
-                .and().build();
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드X, (엑세스토큰+사용자프로필정보 O)
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService)
+                .and().and().build();
     }
 }
